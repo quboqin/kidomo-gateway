2e1d693a92ed5778edb7d78bbc2137bb69889f24
Qubo Qin <quboqin0710@gmail.com>
2024-07-04 19:04:51 +0800

    support cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module'
 import { ConfigService } from './services/config/config.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { cors: true })
   const options = new DocumentBuilder()
     .setTitle('API docs')
     .addTag('users')
